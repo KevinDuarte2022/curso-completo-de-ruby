23fef9af7d46bf6059dcf6169cb28c7d0dacf0e0
Kevin Duarte <elite.kevin.duarte@gmail.com>
2024-07-08 15:43:41 -0300

    Hello name + somar 2 números com Ruby

---
+++ calculoBasico.rb
@@ -0,0 +1,11 @@
+print "Digite o primeiro número inteiro: "
+
+numero1 = gets.chomp.to_i
+
+print "Digite o segundo número inteiro: "
+
+numero2 = gets.chomp.to_i
+
+soma = numero1 + numero2
+
+puts "O resultado da soma dos dois número é #{soma}"
